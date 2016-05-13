e556928456b978e34d7bed5216d3868d70730d69
Thomas Chen <chen.thomas.c@gmail.com>
2016-05-13 17:06:47 -0400

    Add movie model and finish tests

--- test/integration/listing_movies_test.exs
+++ test/integration/listing_movies_test.exs
@@ -2,11 +2,19 @@ defmodule ListingMoviesIntegrationTest do
   use ExUnit.Case, async: true
   use Plug.Test
   alias Watchlist.Router
+  alias Watchlist.Movie
+  alias Watchlist.Repo
 
   @opts Router.init([])
   test 'listing movies' do
+    movies =
+      Repo.all(Movie)
+      |> Poison.encode!
+
     conn = conn(:get, "/movies")
     response = Router.call(conn, @opts)
+
     assert response.status == 200
+    assert response.resp_body == movies
   end
 end
--- web/controllers/movie_controller.ex
+++ web/controllers/movie_controller.ex
@@ -1,7 +1,9 @@
 defmodule Watchlist.MovieController do
   use Watchlist.Web, :controller
+  alias Watchlist.Movie
 
   def index(conn, _params) do
-    render conn, movies: []
+    movies = Repo.all(Movie)
+    render conn, movies: movies
   end
 end
